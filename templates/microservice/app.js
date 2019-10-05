0c1356391b78a57de1a2b7a3222775fe71275c4d
Apostolis21 <apostolis.pyrgos@gmail.com>
2019-10-05 17:33:50 +0300

    add lint and add security lib

--- templates/microservice/app.js
+++ templates/microservice/app.js
@@ -5,12 +5,31 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
+const session = require('express-session');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./controllers/errors/error');
 
 const app = express();
 const testRoutes = require('./routes/test');
 const winston = require('./Utils/logger');
 // view engine setup
+
+// Cross Origin Resource Sharing
+app.use(cors());
+// protect api requests
+app.use(helmet());
+// Preventing HTTP Parameter Pollution
+app.use(hpp());
+
+app.use(session({
+    name: 'SESS_ID',
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  
+  }));
 
 app.use(logger('combined',{ stream: winston.stream }));
 app.use(express.json());
